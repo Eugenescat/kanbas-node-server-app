6ce4981a3dfb48a2db6759d75a96e4a70acb838b
yan <eugenescat@gmail.com>
2024-06-18 07:53:59 +0800

    A6

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
 import express from 'express';
+import mongoose from "mongoose";
+import "dotenv/config";
+import UserRoutes from "./Users/routes.js";
 import cors from "cors";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
 import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
+
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(cors()); // Enable CORS
 app.use(express.json());
+UserRoutes(app);
 Lab5(app);
 Hello(app);
 CourseRoutes(app);
