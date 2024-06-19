28849b48abac8ce51d77a34fcb06be9b200d2e18
yan <eugenescat@gmail.com>
2024-06-19 16:26:49 +0800

    courses crud works

--- Kanbas/Courses/dao.js
+++ Kanbas/Courses/dao.js
@@ -1,6 +1,8 @@
 import CourseModel from "./model.js";
 
 export const createCourse = async (course) => {
+    // 确保 course 对象不包含 _id 字段（can't figure out where I add '_id' from frontend :( temporarily fix it)
+    delete course._id;
     const newCourse = await CourseModel.create(course);
     return newCourse;
 };
